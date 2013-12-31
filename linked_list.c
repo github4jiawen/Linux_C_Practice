529d970519452fdcac6e614a8dd5a3d368ba9440
github4jiawen <591968547@qq.com>
2013-12-31 21:43:46 +0800

    golang

--- linked_list.c
+++ linked_list.c
@@ -8,12 +8,15 @@ int add(int a,int b)
 
 int input(int a,int b)
 {
+	int a,b;
 	printf("we need two number to add\n");
 	scanf("%d %d",&a,&b);
 }
 
 int main(int argc, char const *argv[])
 {
+	input();
+	add();
 	printf("this is the end of this program\n");
 	return 0;
 }
