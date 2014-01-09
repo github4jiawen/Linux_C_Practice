bdaab564074dee4a07507ae9beae02ea99a37040
github4jiawen <591968547@qq.com>
2014-01-09 18:51:58 +0800

    clean

--- main.c
+++ main.c
@@ -1,17 +1,24 @@
 /*main.c*/
-#include "stdlib.h"
+#include <stdio.h>
 #include "binarytree.h"
 
 void print_item(link p)
 {
-	printf("%d\n", p->item);
+	printf("%d",p->item);
 }
 
-int main(int argc, char const *argv[])
+int main()
 {
-	unsigned char pre_seq[]=
-	unsigned char in_seq[]=
+	unsigned char pre_seq[]={4,2,1,3,6,5,7};
+	unsigned char in_seq[]={1,2,3,4,5,6,7,};
 	link root=tree_init(pre_seq,in_seq,7);
-	pre_order
+	pre_order(root,print_item);
+	putchar('\n');
+	in_order(root,print_item);
+	putchar('\n');
+	post_order(root,print_item);
+	putchar('\n');
+	printf("count=%d depth=%d",count(root),depth(root));
+	destroy(root);
 	return 0;
 }
